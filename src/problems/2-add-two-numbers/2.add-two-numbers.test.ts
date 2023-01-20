d76c4eabcf104d9c029e79964c58aca2451200e8
Remo Fiorentino <remo-jose.casadiego@klarna.com>
2023-01-20 19:29:02 +0100

    feat(problems): 2 add two number

---
+++ src/problems/2-add-two-numbers/2.add-two-numbers.test.ts
@@ -0,0 +1,40 @@
+// Input: l1 = [2,4,3], l2 = [5,6,4]
+// Output: [7,0,8]
+// Explanation: 342 + 465 = 807.
+// Example 2:
+
+// Input: l1 = [0], l2 = [0]
+// Output: [0]
+// Example 3:
+
+// Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+// Output: [8,9,9,9,0,0,0,1]
+
+import { addTwoNumbers, ListNode } from "./2.add-two-numbers";
+
+const arr2List = (arr: number[]) => {
+  const firstNode = new ListNode(arr[0]);
+  arr.slice(1).reduce((pv: ListNode, cv: number) => {
+    pv.next = new ListNode(cv);
+    return pv.next;
+  }, firstNode);
+  return firstNode;
+};
+
+describe("given tests", () => {
+  test("given tests 1", () => {
+    expect(
+      addTwoNumbers(arr2List([2, 4, 3]), arr2List([5, 6, 4]))
+    ).toStrictEqual(arr2List([7, 0, 8]));
+  });
+  test("given tests 2", () => {
+    expect(addTwoNumbers(arr2List([0]), arr2List([0]))).toStrictEqual(
+      arr2List([0])
+    );
+  });
+  test("given tests 3", () => {
+    expect(
+      addTwoNumbers(arr2List([9, 9, 9, 9, 9, 9, 9]), arr2List([9, 9, 9, 9]))
+    ).toStrictEqual(arr2List([8, 9, 9, 9, 0, 0, 0, 1]));
+  });
+});
---
+++ src/problems/2-add-two-numbers/2.add-two-numbers.ts
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=2 lang=typescript
+ *
+ * [2] Add Two Numbers
+ */
+
+export class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val = 0, next: ListNode | null = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+type Node = ListNode | null | undefined;
+
+const getSum = (n1?: Node, n2?: Node, complement = 0) => {
+  const number = (n1?.val || 0) + (n2?.val || 0) + complement;
+  return { val: number % 10, residual: number >= 10 ? 1 : 0 };
+};
+
+function addTwoNumbers(l1: Node, l2: Node): ListNode {
+  const sumNodes = (n1: Node, n2: Node, complement = 0, lastNode: ListNode) => {
+    if (n1 === null && n2 === null) {
+      if (complement === 1 && lastNode)
+        lastNode.next = new ListNode(complement);
+      return;
+    }
+    const { val, residual } = getSum(n1, n2, complement);
+    lastNode.next = new ListNode(val);
+    sumNodes(n1?.next || null, n2?.next || null, residual, lastNode.next);
+  };
+  const firstSum = getSum(l1, l2);
+  const head = new ListNode(firstSum.val);
+  sumNodes(l1?.next, l2?.next, firstSum.residual, head);
+  return head;
+}
+// @lc code=end
+
+export { addTwoNumbers };
